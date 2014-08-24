e701e5c47b2b2e2707cb6cf19700f730cdde886e
Noah <noah@digitalmockingbird.com>
2014-08-24 01:57:15 -0700

    maybe this?

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -100,10 +100,10 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = '{{ project_name }}.urls'
+ROOT_URLCONF = 'tango_with_django_project.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
+WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
 TEMPLATE_DIRS = (
     local_path('templates/')
