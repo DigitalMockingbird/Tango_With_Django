eaf1a0aca6a2a29316ed7eed571c21044ea33490
Noah <noah@digitalmockingbird.com>
2014-08-24 02:40:34 -0700

    hi

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -5,7 +5,7 @@ import dj_database_url
 boolean = lambda value: bool(int(value))
 PROJECT_DIR = os.path.dirname(__file__)
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_DIR, 'templates')
+    os.path.join(BASE_DIR, 'templates')
 )
 
 # DEBUG = boolean(os.environ.get('DEBUG', 0))
@@ -64,7 +64,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.environ.get('MEDIA_ROOT', PROJECT_DIR('media/'))
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', BASE_DIR('media/'))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -75,7 +75,7 @@ MEDIA_URL = os.environ.get('MEDIA_URL', '/media/')
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.environ.get('STATIC_ROOT', PROJECT_DIR('static/'))
+STATIC_ROOT = os.environ.get('STATIC_ROOT', BASE_DIR('static/'))
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
