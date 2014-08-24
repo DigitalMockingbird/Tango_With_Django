3c3eb4a07b65fe05c5d6af855d6c6a91f8bc7c21
Noah <noah@digitalmockingbird.com>
2014-08-24 03:05:25 -0700

    again!

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = ['*']
 # STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(local_path, 'static'),
 )
 
 # Local time zone for this installation. Choices can be found here:
@@ -81,7 +81,7 @@ MEDIA_URL = os.environ.get('MEDIA_URL', '/media/')
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.environ.get('STATIC_ROOT', BASE_DIR('static/'))
+STATIC_ROOT = os.environ.get('STATIC_ROOT', local_path('static/'))
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
